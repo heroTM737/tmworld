8f1b8aae514589a04211a4ee5dcfa78a253acf0d
heroTM737 <xuantien.nguyen93@gmail.com>
2015-09-09 00:49:48 +0700

    image view

--- app/what_a_beauty/tab.js
+++ app/what_a_beauty/tab.js
@@ -1,10 +1,42 @@
-$(document).ready(function(){
-    $.each($(".tab"), function(index, obj){
-        $(obj).children(".tab-title").click(function(){
-            $(".tab .tab-content").attr("style","display:none");
-            $(obj).children(".tab-content").attr("style","display:block");
+
+
+$(document).ready(function () {
+    //create default data
+    var list = ["dota2_1", "juggernaut_1", "juggernaut_2", "juggernaut_3", "juggernaut_4", "lina_1", "razor_1", "traxex_1"];
+    
+    var template = $(".menu").html();
+    
+    $(".menu").html("");
+    
+    for (var i in list){
+        var current = template.slice(0);
+        current = current.replace("nameputhere", list[i]);
+        current = current.replace("nameputhere", list[i]);
+        $(".menu").append(current);
+    }
+    
+    //assign event for each tab
+    $.each($(".tab"), function (index, obj) {
+        $(obj).children(".tab-title").click(function () {
+            openTab(obj);
         });
     });
-    
-    $(".tab .tab-content").first().attr("style","display:block");
+
+    //open first tab as default
+    openTab($(".tab").first());
 })
+
+function openTab(obj) {
+    $(".tab .tab-content").attr("style", "display:none");
+
+    //load tab content if not
+    var html = $(obj).children(".tab-content").html();
+    if (html.indexOf("<xmp>") >= 0) {
+        html = html.replace("<xmp>", "");
+        html = html.replace("</xmp>", "");
+        $(obj).children(".tab-content").html(html);
+    }
+
+    $(obj).children(".tab-content").attr("style", "display:block");
+
+}
