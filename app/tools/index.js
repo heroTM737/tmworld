947267d50987aae665af544a993741c111cc7147
heroTM737 <xuantien.nguyen93@gmail.com>
2016-10-18 11:46:30 +0700

    update HTML Entities

--- app/tools/index.js
+++ app/tools/index.js
@@ -1,3 +1,4 @@
+//RGB - Hex
 function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
@@ -38,4 +39,50 @@ function convertHex2RGB() {
     $("#red").val(rgb.r);
     $("#green").val(rgb.g);
     $("#blue").val(rgb.b);
+}
+
+//HTML Entities
+function decodeHTMLEntities(str) {
+    str = str.split("&nbsp;").join(" ");
+    str = str.split("&lt;").join("<");
+    str = str.split("&gt;").join(">");
+    str = str.split("&amp;").join("&");
+    str = str.split("&quot;").join("\"\"");
+    str = str.split("&apos;").join("''");
+    str = str.split("&cent;").join("¢");
+    str = str.split("&pound;").join("¢");
+    str = str.split("&yen;").join("¥");
+    str = str.split("&euro;").join("€");
+    str = str.split("&copy;").join("©");
+    str = str.split("&reg;").join("®");
+    return str;
+}
+
+function encodeHTMLEntities(str) {
+    str = str.split(" ").join("&nbsp;");
+    str = str.split("<").join("&lt;");
+    str = str.split(">").join("&gt;");
+    str = str.split("&").join("&amp;");
+    str = str.split("\"\"").join("&quot;");
+    str = str.split("''").join("&apos;");
+    str = str.split("¢").join("&cent;");
+    str = str.split("¢").join("&pound;");
+    str = str.split("¥").join("&yen;");
+    str = str.split("€").join("&euro;");
+    str = str.split("©").join("&copy;");
+    str = str.split("®").join("&reg;");
+    return str;
+}
+
+function doEncodeHTMLEntities() {
+    var decoded = $("#HTMLDecodedText").val();
+    console.log(decoded);
+    var encoded = encodeHTMLEntities(decoded);
+    $("#HTMLEncodedText").val(encoded);
+}
+
+function doDecodeHTMLEntities() {
+    var encoded = $("#HTMLEncodedText").val();
+    var decoded = decodeHTMLEntities(encoded);
+    $("#HTMLDecodedText").val(decoded);
 }
