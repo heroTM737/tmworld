cb4846b80f39f253bc13d4bc130fe630ce6d4026
heroTM737 <xuantien.nguyen93@gmail.com>
2017-04-22 21:43:40 +0700

    update warfriends detail

--- game/warfriends/src/components/table.js
+++ game/warfriends/src/components/table.js
@@ -8,6 +8,8 @@ export default class Table extends Component {
 
     render() {
         var data = this.props.data;
+        var firstEntry = data[Object.keys(data)[0]];
+        var isArmy = firstEntry.min_health != undefined && firstEntry.min_health != null;
 
         var children = [];
         for (var i in data) {
@@ -17,6 +19,16 @@ export default class Table extends Component {
                     <td>{data[i].price_type == "gold" ? null : data[i].price}</td>
                     <td>{data[i].price_type == "gold" ? data[i].price : null}</td>
                     <td>{data[i].level}</td>
+                    <td>{data[i].min_damage}</td>
+                    <td>{data[i].max_damage}</td>
+                    <td>{data[i].accuracy}</td>
+                    <td>{data[i].rate_of_fire}</td>
+                    <td>{data[i].shot_speed}</td>
+                    { isArmy ? <td>{data[i].min_health}</td> : null}
+                    { isArmy ? <td>{data[i].max_health}</td> : null}
+                    { isArmy ? <td>{data[i].deploy_cost}</td> : null}
+                    { isArmy ? <td>{data[i].deploy_cooldown}</td> : null}
+                    { isArmy ? <td>{data[i].move_speed}</td> : null}
                 </tr>
             );
         }
@@ -29,6 +41,16 @@ export default class Table extends Component {
                         <th>Warbucks</th>
                         <th>Gold</th>
                         <th>Rank</th>
+                        <th>Min Damage</th>
+                        <th>Max Damage</th>
+                        <th>Accuracy (%)</th>
+                        <th>Rate of fire (/m)</th>
+                        <th>Shot speed</th>
+                        { isArmy ? <th>Min Health</th> : null}
+                        { isArmy ? <th>Max Health</th> : null}
+                        { isArmy ? <th>Deploy Cost</th> : null}
+                        { isArmy ? <th>Deploy Cooldown</th> : null}
+                        { isArmy ? <th>Move Speed</th> : null}
                     </tr>
                 </thead>
                 <tbody>
