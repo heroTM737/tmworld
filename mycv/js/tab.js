3fbde27e7f003f2396c5f450f435e591787202b4
heroTM737 <xuantien.nguyen93@gmail.com>
2017-12-07 14:35:30 +0700

    update mycv

---
+++ mycv/js/tab.js
@@ -0,0 +1,23 @@
+function createTabs() {
+    $(".tabs").each(function (tabContainerIndex, tabContainer) {
+        var firstId = $(this).find("li").first().attr("data-id");
+        openTab(firstId);
+
+        $(this).find("li").each(function (liIndex, li) {
+            $(li).click(function () {
+                openTab($(li).attr("data-id"));
+            });
+        });
+
+        function openTab(id) {
+            $(tabContainer).find(".tab").each(function (tabIndex, tab) {
+                if ($(tab).attr("data-id") == id) {
+                    $(tab).css("display", "block");
+                } else {
+                    $(tab).css("display", "none");
+                }
+            });
+        }
+
+    });
+}
