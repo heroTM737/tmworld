e7df87d8e072193e629954dfaff24e08a0c2fdda
heroTM737 <xuantien.nguyen93@gmail.com>
2017-12-07 15:52:06 +0700

    update style

--- mycv/index.js
+++ mycv/index.js
@@ -1,3 +1,14 @@
 $(function () {
     createTabs();
+
+    $(window).scroll(function () {
+        //remove all active
+        $("#menu li").children().removeClass("active");
+
+        //active current id
+        var scroll = $(window).scrollTop();
+        var wh = $(window).height();
+        var index = parseInt(scroll / wh);
+        $("#menu li").children().eq(index).addClass("active");
+    });
 });
