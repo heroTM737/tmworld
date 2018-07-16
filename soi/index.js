01236999975bbc2211d9c1718c3450fc22d2db41
Nguyễn Xuân Tiến <tiennx1@fsoft.com.vn>
2018-07-16 10:42:05 +0700

    update soi style

--- soi/index.js
+++ soi/index.js
@@ -24,3 +24,35 @@ setInterval(function () {
     $('#second').html(second);
 
 }, 1000);
+
+let rate = 0.1;
+// let rate = 1;
+$(document).ready(function () {
+    $('.hover-container').on('mousemove', function (event) {
+        let containerW = $(this).outerWidth();
+        let containerH = $(this).outerHeight();
+        let containerT = $(this).offset().top;
+        let containerL = $(this).offset().left;
+
+        let content = $('.hover-content');
+
+        let centerY = containerT + containerH / 2;
+        let top = event.pageY - centerY;
+        if (top < -50) {
+            top = -50;
+        } else if (top > 0) {
+            top = 0;
+        }
+
+        let centerX = containerL + containerW / 2;
+        let left = event.pageX - centerX;
+        if (left < -50) {
+            left = -50;
+        } else if (left > 0) {
+            left = 0;
+        }
+
+        content.css('top', top + 'px');
+        content.css('left', left + 'px');
+    });
+});
