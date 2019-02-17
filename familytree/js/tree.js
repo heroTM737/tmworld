4f34402fbdf3ca824c38ce7947af9c41f68fb568
Nguyễn Xuân Tiến <xuantien.nguyen93@gmail.com>
2019-02-17 21:54:57 +0700

    adđ familytree

---
+++ familytree/js/tree.js
@@ -0,0 +1,576 @@
+var treeData = {
+    id: "tien",
+    name: "Vũ Nguyên Ngọc - Vũ Thị Vui",
+    children: [
+        {
+            id: "js",
+            name: "Vũ Thanh Phông - Nguyễn Thị Thu Hương",
+            children: [
+                {
+                    id: "reactjs",
+                    name: "Vũ Thu Hiền (Cún)"
+                },
+                {
+                    id: "angularjs",
+                    name: "Vũ Tố Quyên (Kẹo)",
+                },
+            ]
+        },
+        {
+            name: "Vũ Văn Nam - Lý",
+            children: [
+                {
+                    id: "sass",
+                    name: "Trang"
+                },
+                {
+                    id: "bootstrap",
+                    name: "Hưng"
+                },
+                {
+                    name: "Chưa gặp chưa biết tên"
+                }
+            ]
+        },
+        {
+            id: "java",
+            name: "Vũ Xuân Tam - Gấm",
+            children: [
+                {
+                    id: "javaswing",
+                    name: "Đảo"
+                },
+                {
+                    id: "gwt",
+                    name: "Tuấn Anh"
+                },
+            ]
+        },
+        {
+            id: "d3js",
+            name: "Vũ Hùng Tráng - Phương",
+            children: [
+                {
+                    name: "? (Nhím)"
+                },
+                {
+                    name: "? (Bi)"
+                }
+            ]
+        },
+    ]
+}
+
+var motherTreeData = {
+    name: "Nguyễn Thế Vinh - Vũ Thị Vy",
+    children: [
+        {
+            name: "Nguyễn Thị Thanh Hà - Mác",
+            children: [
+                {
+                    name: "Phạm Thanh Thủy - Nguyễn Phan Hưng",
+                    children: [
+                        {
+                            name: "Nguyễn Thủy Du (Nem)"
+                        },
+                        {
+                            name: "Nguyễn Phan Phan (Ớt)"
+                        },
+                    ]
+                },
+                {
+                    name: "Phạm Minh Thành - Hiền"
+                },
+                {
+                    name: "Phạm Minh Trung - Phương",
+                    children: [
+                        {
+                            name: "Phạm Phương Thu (Ốc)"
+                        },
+                        {
+                            name: "Phạm Minh Hiếu (Bia)"
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Thị Lệ Hằng - Nguyễn Quốc Thịnh",
+            children: [
+                {
+                    name: "Nguyễn Anh Tuấn - Hương",
+                    children: [
+                        {
+                            name: "? (mimi)"
+                        },
+                    ]
+                },
+                {
+                    name: "Nguyễn Tuấn Long"
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Thị Thu Hương - Phông",
+            children: [
+                {
+                    id: "reactjs",
+                    name: "Vũ Thu Hiền (Cún)"
+                },
+                {
+                    id: "angularjs",
+                    name: "Vũ Tố Quyên (Kẹo)",
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Thế Quang - Huệ, Hà, Hà",
+            children: [
+                {
+                    name: "Nguyễn ? Giang"
+                },
+                {
+                    name: "Nguyễn Phương Mai (Tôm)"
+                },
+                {
+                    name: "Nguyễn ? ? (Tép)"
+                },
+            ]
+        },
+    ]
+}
+
+var tienFatherTreeData = {
+    name: "Nguyễn Lợi Liêm - Nguyễn Thị Quyết",
+    children: [
+        {
+            name: "Nguyễn Thị Nhâm - Nghĩa",
+            children: [
+                {
+                    name: "Sơn - Huyền",
+                    children: [
+                        {
+                            name: "? (Cún)"
+                        },
+                    ]
+                },
+                {
+                    name: "Hồng - Hồng",
+                    children: [
+                        {
+                            name: "Ngọc"
+                        },
+                        {
+                            name: "?"
+                        },
+                    ]
+                },
+                {
+                    name: "Thủy - Thu",
+                    children: [
+                        {
+                            name: "Duy (ba dưa)"
+                        },
+                    ]
+                },
+                {
+                    name: "Hằng - Dinh",
+                    children: [
+                        {
+                            name: "Long"
+                        },
+                        {
+                            name: "Ly (chích bông)"
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Thị Minh",
+            children: [
+                {
+                    name: "Thắng - Hà",
+                    children: [
+                        {
+                            name: "Duy"
+                        },
+                        {
+                            name: "?"
+                        },
+                    ]
+                },
+                {
+                    name: "Lợi - Diệp",
+                    children: [
+                        {
+                            name: "? (báo)"
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: "Nguyễn ? Khánh"
+        },
+        {
+            name: "Nguyễn ? Hải - Lan",
+            children: [
+                {
+                    name: "Hiền - Hưng",
+                    children: [
+                        {
+                            name: "? (nhím)"
+                        },
+                        {
+                            name: "? (nấm)"
+                        },
+                    ]
+                },
+                {
+                    name: "Tùng - Yến",
+                    children: [
+                        {
+                            name: "? (dế)"
+                        },
+                        {
+                            name: "? (giun)"
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Xuân Vĩnh - Sơn",
+            children: [
+                {
+                    name: "Nguyễn Lâm Giang - Nguyễn Hải Hà",
+                    children: [
+                        {
+                            name: "Nguyễn Hoàng Dũng (bin)"
+                        },
+                        {
+                            name: "Nguyễn Hồng Nhung (chíp)"
+                        },
+                    ]
+                },
+                {
+                    name: "Nguyễn Xuân Tiến"
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Thị Mùi - ?, Hùng",
+            children: [
+                {
+                    name: "Thảo (bột) - Long",
+                    children: [
+                        {
+                            name: "? (mèo)"
+                        },
+                        {
+                            name: "? (lion)"
+                        },
+
+                    ]
+                },
+                {
+                    name: "Hà Anh (hành)"
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Mạnh Hùng - Hương",
+            children: [
+                {
+                    name: "Nguyễn Mạnh Linh",
+                    children: [
+                        {
+                            name: "? (cà)"
+                        },
+                    ]
+                },
+                {
+                    name: "Nguyễn ? Quyên"
+                },
+            ]
+        },
+        {
+            name: "Nguyễn Mạnh Hướng - Hoan",
+            children: [
+                {
+                    name: "Việt Anh (bốp)"
+                },
+                {
+                    name: "? (bống)"
+                },
+            ]
+        }
+    ]
+}
+var tienMotherTreeData = {
+    name: "Trần Quang Bình - Chu Thị Cứu",
+    children: [
+        {
+            name: "Hoàn - Sơn",
+            children: [
+                {
+                    name: "Hà - Nam",
+                    children: [
+                        {
+                            name: "Yến Nhi"
+                        },
+                        {
+                            name: "Quỳnh Anh"
+                        },
+                        {
+                            name: "Nam Dũng"
+                        },
+                    ]
+                },
+                {
+                    name: "Hải - Đào"
+                },
+            ]
+        },
+        {
+            name: "Trần Thị Sơn - Vĩnh",
+            children: [
+                {
+                    name: "Nguyễn Lâm Giang - Nguyễn Hải Hà",
+                    children: [
+                        {
+                            name: "Nguyễn Hoàng Dũng (bin)"
+                        },
+                        {
+                            name: "Nguyễn Hồng Nhung (chíp)"
+                        },
+                    ]
+                },
+                {
+                    name: "Nguyễn Xuân Tiến"
+                },
+            ]
+        },
+        {
+            name: "Bích - Vân",
+            children: [
+                {
+                    name: "Ngọc"
+                },
+            ]
+        },
+        {
+            name: "Lộc - Thái",
+            children: [
+                {
+                    name: "Kiên - Lan Anh"
+                },
+                {
+                    name: "Quỳnh - Tùng",
+                    children: [
+                        {
+                            name: "Khánh An"
+                        },
+                        {
+                            name: "Khang"
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            name: "Nha - Yến",
+            children: [
+                {
+                    name: "Trường"
+                },
+                {
+                    name: "Thọ"
+                },
+            ]
+        },
+        {
+            name: "Huấn  - Hà",
+            children: [
+                {
+                    name: "Hiếu"
+                },
+                {
+                    name: "Quân"
+                },
+            ]
+        },
+        {
+            name: "Tuấn - Thúy",
+            children: [
+                {
+                    name: "Tú"
+                },
+                {
+                    name: "Linh"
+                },
+            ]
+        },
+        
+        {
+            name: "Tân - Tiến",
+            children: [
+                {
+                    name: "Đức"
+                },
+                {
+                    name: "Trung"
+                },
+            ]
+        },
+        {
+            name: "Trọng - Yến",
+            children: [
+                {
+                    name: "Nghĩa"
+                },
+            ]
+        },
+    ]
+}
+createTree(treeData, "noi");
+createTree(motherTreeData, "ngoai");
+createTree(tienFatherTreeData, "tien-noi");
+createTree(tienMotherTreeData, "tien-ngoai");
+
+function createTree(data, containerid) {
+    var margin = { top: 20, right: 120, bottom: 20, left: 120 },
+        width = 800 - margin.right - margin.left,
+        height = 800 - margin.top - margin.bottom;
+
+    var i = 0,
+        duration = 750,
+        root;
+
+    var tree = d3.layout.tree()
+        .size([height, width]);
+
+    var diagonal = d3.svg.diagonal()
+        .projection(function (d) { return [d.y, d.x]; });
+
+    var svg = d3.select("#" + containerid).append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    root = data;
+    root.x0 = height / 2;
+    root.y0 = 0;
+
+    update(root);
+
+    d3.select(self.frameElement).style("height", "800px");
+
+    function update(source) {
+
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function (d) { d.y = d.depth * 180; });
+
+        // Update the nodes…
+        var node = svg.selectAll("g.node")
+            .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+        // Enter any new nodes at the parent's previous position.
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+            .on("click", click);
+
+        nodeEnter.append("circle")
+            .attr("r", 1e-6);
+
+        nodeEnter.append("text")
+            .attr("class", "name")
+            .attr("x", function (d) { return d.children || d._children ? -22 : 22; })
+            .attr("dy", ".35em")
+            .attr("text-anchor", function (d) { return d.children || d._children ? "end" : "start"; })
+            .text(function (d) { return d.name; })
+            .style("fill-opacity", 1e-6);
+
+        nodeEnter.append("text")
+            .attr("class", "shortname")
+            .attr("x", function (d) { return 0; })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .text(function (d) { return d.name; })
+            .style("fill-opacity", 1e-6);
+
+        // Transition nodes to their new position.
+        var nodeUpdate = node.transition()
+            .duration(duration)
+            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+        nodeUpdate.select("circle")
+            .attr("r", 17);
+
+        nodeUpdate.selectAll("text")
+            .style("fill-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        var nodeExit = node.exit().transition()
+            .duration(duration)
+            .attr("transform", function (d) { return "translate(" + source.y + "," + source.x + ")"; })
+            .remove();
+
+        nodeExit.select("circle")
+            .attr("r", 1e-6);
+
+        nodeExit.select("text")
+            .style("fill-opacity", 1e-6);
+
+        // Update the links…
+        var link = svg.selectAll("path.link")
+            .data(links, function (d) { return d.target.id; });
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                var o = { x: source.x0, y: source.y0 };
+                return diagonal({ source: o, target: o });
+            });
+
+        // Transition links to their new position.
+        link.transition()
+            .duration(duration)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition()
+            .duration(duration)
+            .attr("d", function (d) {
+                var o = { x: source.x, y: source.y };
+                return diagonal({ source: o, target: o });
+            })
+            .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach(function (d) {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
+
+    // Toggle children on click.
+    function click(d) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+        update(d);
+    }
+}
